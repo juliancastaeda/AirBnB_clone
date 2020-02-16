9461d7febe0be2213fce4dd212c4b0a6197d03e7
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-02-16 00:36:39 +0000

    add class BaseModel with Public instance attributes id

--- models/base_model.py
+++ models/base_model.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python3
 # description of the function
+"""
+Base Models
+"""
+""" import """
+from datetime import datetime, date, time
+import uuid
+
+"""
+class BaseModel: attributes/methods
+"""
+class BaseModel:
+    """ Public instance attributes """
+    id = str(uuid.uuid4())
+    created_at = datetime.now(tz=None)
+    updated_at = datetime.now(tz=None)
