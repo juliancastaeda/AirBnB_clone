c1f0362465987946b6672e25ad95d5c2da811f1c
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-02-16 00:54:13 +0000

    add function should print: [BaseModel] str(self.id) self.__dict__

--- models/base_model.py
+++ models/base_model.py
@@ -3,15 +3,22 @@
 """
 Base Models
 """
-""" import """
-from datetime import datetime, date, time
+from datetime import datetime
 import uuid
 
-"""
-class BaseModel: attributes/methods
-"""
+
+
 class BaseModel:
+    """
+    class BaseModel: attributes/methods
+    """
     """ Public instance attributes """
     id = str(uuid.uuid4())
     created_at = datetime.now(tz=None)
     updated_at = datetime.now(tz=None)
+
+    def __str__(self):
+        """ should print: [<class name>] (<self.id>) <self.__dict__> """
+        """ should print: [BaseModel] str(self.id) self.__dict__ """
+        return "[()] ({}) {}".format(self.__class__.__name__, self.id,
+                                     self.__dict__)
