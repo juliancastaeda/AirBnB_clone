780388d8a97b1b25b472473663400f1e82b8addf
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-02-17 06:05:52 +0000

    [TASK 7 Incomplete] Console 0.1 Creates a new instance of BaseModel

--- console.py
+++ console.py
@@ -2,10 +2,13 @@
 # description of the function
 import cmd
 from models.base_model import BaseModel
+# from models import storage
+import models
 
 class HBNBCommand(cmd.Cmd):
     intro = 'Welcome to the hbnb shell.   Type help or ? to list commands.\n'
     prompt = '(hbnb) '
+    __class_name = {"BaseModel": BaseModel}
 
     # ----- basic hbnb commands -----
     def do_EOF(self, line):
@@ -20,6 +23,17 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
       """ an empty line + ENTER """
       pass
+
+    def do_create(self, line):
+        """ Creates a new instance of BaseModel """
+        if line is None or line == "":
+            print("** class name missing **")
+        elif line not in self.__class_name:
+            print("** class doesn't exist **")
+        else:
+            new_model = self.__class_name[line]()
+            new_model.save()
+            print(new_model.id)
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
