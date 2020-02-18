4fc0ac117e05feedd1d95908bc4bdeed0bd0f60d
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-02-18 05:31:41 +0000

    [Files task 8 and 9] update

--- console.py
+++ console.py
@@ -44,8 +44,6 @@ class HBNBCommand(cmd.Cmd):
     def do_show(self, line):
         """ Prints the string representation of an
         instance based on the class name and id """
-        key = "{}.{}".format(line.split(' ')[0], line.split(' ')[1])
-        objects = models.storage.all()
         if line is None or line == "":
             print("** class name missing **")
         elif line.split(' ')[0] not in self.__class_name:
@@ -53,6 +51,8 @@ class HBNBCommand(cmd.Cmd):
         elif len(line.split(' ')) < 2:
             print("** instance id missing **")
         else:
+            key = "{}.{}".format(line.split(' ')[0], line.split(' ')[1])
+            objects = models.storage.all()
             if key not in objects:
                 print("** no instance found **")
             else:
@@ -60,19 +60,60 @@ class HBNBCommand(cmd.Cmd):
 
     def do_destroy(self, line):
         """ Deletes an instance based on the class name and id """
-        key = "{}.{}".format(line.split(' ')[0], line.split(' ')[1])
+        if line is None or line == "":
+            print("** class name missing **")
+        elif line.split(' ')[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        elif len(line.split(' ')) < 2:
+            print("** instance id missing **")
+        else:
+            key = "{}.{}".format(line.split(' ')[0], line.split(' ')[1])
+            objects = models.storage.all()
+            if k not in objects:
+                print("** no instance found **")
+            else:
+                del objects[key]
+                models.storage.save()
+
+    def do_all(self, line):
+        """ Prints all string representation of all
+        instances based or not on the class name """
         objects = models.storage.all()
+        data_json = []
+        if line == '':
+            for value in objects.values():
+                data_json.append(str(value))
+            print(data_json)
+        elif line in self.__class_name:
+            for value in objects.values():
+                if value.__class__.__name__ == line:
+                    data_json.append(str(value))
+            print(data_json)
+        else:
+            print("** class doesn't exist **")
+
+    def do_update(self, line):
+        """  Updates an instance based on the class name
+        and id by adding or updating attribute
+        (save the change into the JSON file """
         if line is None or line == "":
             print("** class name missing **")
         elif line.split(' ')[0] not in self.__class_name:
             print("** class doesn't exist **")
         elif len(line.split(' ')) < 2:
             print("** instance id missing **")
+        elif len(line.split(' ')) < 3:
+            print("** attribute name missing **")
+        elif len(line.split(' ')) < 4:
+            print("** value missing **")
         else:
-            if key not in objects:
+            key = "{}.{}".format(line.split(' ')[0], line.split(' ')[1])
+            if key not in models.storage.all():
                 print("** no instance found **")
             else:
-                print(objects[key])
+                line_1 = line.split()[2]
+                value = line.split()[3]
+                setattr(models.storage.all()[key], line_1, value)
                 models.storage.save()
 
     def do_all(self, line):
@@ -90,30 +131,13 @@ class HBNBCommand(cmd.Cmd):
             data_json = [str(value) for value in objects.values()]
             print(data_json)
 
-    def do_update(self, line):
-        """  Updates an instance based on the class name
-        and id by adding or updating attribute
-        (save the change into the JSON file """
-        objects = models.storage.all()
-        if line is None or line == "":
-            print("** class name missing **")
-        elif line.split(' ')[0] not in self.__class_name:
-            print("** class doesn't exist **")
-        elif len(line.split(' ')) < 2:
-            print("** instance id missing **")
-        elif len(line.split(' ')) < 3:
-            print("** attribute name missing **")
-        elif len(line.split(' ')) < 4:
-            print("** value missing **")
-        else:
-            key = "{}.{}".format(line.split(' ')[0], line.split(' ')[1])
-            if key not in objects:
-                print("** no instance found **")
-            else:
-                llne = line.split()[2]
-                value = line.split()[3]
-                setattr(objects[key], line, value)
-                models.storage.save()
+    def do_count(self, line):
+        """Count command counts the instances of a class"""
+        count = 0
+        for key, value in models.storage.all().items():
+            if key.split(".")[0] == line:
+                count += 1
+        print(count)
 
     def default(self, line):
         if (re.match(r"\w+\.\w+(\(\)|\(\"[^\"]*\"(?:, (\"[^\"]*\"|{.*}))*\))",
@@ -126,6 +150,8 @@ class HBNBCommand(cmd.Cmd):
                 self.do_all(lines[0])
             elif line[1][0:5] == "show(" and line[1][-1:] == ")":
                 self.do_show(line[0] + " " + line[1][5:-1])
+            elif lines[1] == "count()":
+                self.do_count(lines[0])
             elif lines[1][0:8] == "destroy(" and lines[1][-1:] == ")":
                 self.do_destroy(lines[0] + " " + lines[1][8:-1])
         else:
