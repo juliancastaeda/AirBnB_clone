5a84a4a4ac15bdae984f66a098042e542c9a83c8
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-02-18 15:32:45 +0000

    [Files task 8 and 9] update

--- models/base_model.py
+++ models/base_model.py
@@ -30,7 +30,7 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             models.storage.new(self)
-            models.storage.save()
+            # models.storage.save()
     # created_at = datetime.strptime(created_at, "%Y-%m-%dT%H:%M:%S.%f")
     # updated_at = datetime.strptime(updated_at, "%Y-%m-%dT%H:%M:%S.%f")
 
@@ -44,7 +44,7 @@ class BaseModel:
         """ updates the public instance attribute updated_at
         with the current datetime """
         self.updated_at = datetime.now()
-        # models.storage.new(self)
+        models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
