a7039e316d7f3d40b78331fd72364ec61660a72e
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-02-19 02:16:56 +0000

    [Pep8] fix line

--- models/base_model.py
+++ models/base_model.py
@@ -17,11 +17,11 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """ """
         if kwargs:
-            for key, value in kwargs.items():
+            for key, val in kwargs.items():
                 if key != "__class__":
                     if key == "created_at" or key == "updated_at":
-                        value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                    setattr(self, key, value)
+                        val = datetime.strptime(val, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, val)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
