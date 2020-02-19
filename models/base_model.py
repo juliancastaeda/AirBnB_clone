49e87a78251e75c6fb894c787eb2fbdeeee9db2b
juliancastaeda <1277@holbertonschool.com>
2020-02-18 21:00:44 -0500

    upload file

--- models/base_model.py
+++ models/base_model.py
@@ -20,7 +20,7 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key != "__class__":
                     if key == "created_at" or key == "updated_at":
-                        value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                        valu = datetime.strptime(valu, '%Y-%m-%dT%H:%M:%S.%f')
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
@@ -32,7 +32,7 @@ class BaseModel:
         """ should print: [<class name>] (<self.id>) <self.__dict__> """
         """ should print: [BaseModel] str(self.id) self.__dict__ """
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
-                                         self.__dict__)
+                                     self.__dict__)
 
     def save(self):
         """ updates the public instance attribute updated_at
