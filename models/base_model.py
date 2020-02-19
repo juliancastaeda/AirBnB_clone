f7d487476bbdfc39e282db6d5b12941f752aad8b
juliancastaeda <1277@holbertonschool.com>
2020-02-18 21:24:04 -0500

    load file

--- models/base_model.py
+++ models/base_model.py
@@ -17,10 +17,10 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """ """
         if kwargs:
-            for key, value in kwargs.items():
+            for key, valu in kwargs.items():
                 if key != "__class__":
                     if key == "created_at" or key == "updated_at":
-                        valu = datetime.strptime(valu, '%Y-%m-%dT%H:%M:%S.%f')
+                        valu = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
                     setattr(self, key, valu)
         else:
             self.id = str(uuid.uuid4())
