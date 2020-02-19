671d6b7a9300ef20d4dd340aba34907e62cc3403
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-02-19 01:44:56 +0000

    [test] change

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
-""" coment """
+from models import base_model
+from models import user
+from models import state
+from models import city
+from models import amenity
+from models import place
+from models import review
 from models.engine.file_storage import FileStorage
+
 storage = FileStorage()
 storage.reload()
