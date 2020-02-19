95a0ae752f0d4a7a9353e0ca758d35a8814ad32c
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-02-19 17:51:44 +0000

    [BaseModel] self.created_at

--- models/base_model.py
+++ models/base_model.py
@@ -16,11 +16,12 @@ class BaseModel:
     """ Public instance attributes """
     def __init__(self, *args, **kwargs):
         """ """
-        if kwargs:
+        if len(kwargs) > 0:
             for key, val in kwargs.items():
-                if key != "__class__":
-                    if key == "created_at" or key == "updated_at":
-                        val = datetime.strptime(val, '%Y-%m-%dT%H:%M:%S.%f')
+                if key == "created_at" or key == "updated_at":
+                    val = datetime.strptime(val, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, val)
+                elif key != "__class__":
                     setattr(self, key, val)
         else:
             self.id = str(uuid.uuid4())
@@ -43,7 +44,7 @@ class BaseModel:
     def to_dict(self):
         """"function comment"""
         dict1 = self.__dict__.copy()
-        dict1["__class__"] = type(self).__name__
+        dict1["__class__"] = self.__class__.__name__
         dict1["created_at"] = dict1["created_at"].isoformat()
         dict1["updated_at"] = dict1["updated_at"].isoformat()
         return dict1
