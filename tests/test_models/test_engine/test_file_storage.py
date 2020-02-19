6430c879a3e3a833c4bfa022253c9751e615750e
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-02-19 17:10:59 +0000

    [2. Unittests] Test base and file storage add import

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
-"""test for file storage"""
+"""Test File Storage"""
 import unittest
 import pep8
 import json
-import os
 import uuid
 from datetime import datetime
+import os
 from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
 from models.engine.file_storage import FileStorage
+from models.user import User
 
 
 class TestFileStorage(unittest.TestCase):
@@ -40,7 +35,7 @@ class TestFileStorage(unittest.TestCase):
     def tearDown(self):
         """teardown"""
         try:
-            os.remove("data.json")
+            os.remove("file.json")
         except Exception:
             pass
 
@@ -99,7 +94,7 @@ class TestFileStorage(unittest.TestCase):
             lines = f.readlines()
         try:
             os.remove(path)
-        except:
+        except Exception:
             pass
         self.storage.save()
         with open(path, 'r') as f:
@@ -107,7 +102,7 @@ class TestFileStorage(unittest.TestCase):
         self.assertEqual(lines, lines2)
         try:
             os.remove(path)
-        except:
+        except Exception:
             pass
         with open(path, "w") as f:
             f.write("{}")
@@ -115,3 +110,24 @@ class TestFileStorage(unittest.TestCase):
             for line in r:
                 self.assertEqual(line, "{}")
         self.assertIs(self.storage.reload(), None)
+
+    def test_save(self):
+        """
+        Testing the save method
+        """
+        bm = BaseModel()
+        bm.save()
+        self.assertTrue(os.path.exists(self.path))
+        bm.name = "Testing"
+        bm.number = 1
+        bm.save()
+        self.assertTrue(os.path.exists(self.path))
+        dic = {}
+        with open('data.json', 'r') as fjson:
+            dic = json.loads(fjson.read())
+        bm_key = bm.__class__.__name__ + '.' + bm.id
+        self.assertDictEqual(bm.to_dict(), dic[bm_key])
+
+
+if __name__ == "__main__":
+    unittest.main()
